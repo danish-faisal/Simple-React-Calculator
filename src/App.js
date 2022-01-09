c51184579caefec55f876eabb2006f4f4e5db7b7
= <danishfaisal.wwe@gmail.com>
2022-01-09 18:02:51 +0530

    CSS styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import "./styles.css";
+
 function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">1234 *</div>
+        <div className="current-operand">1234</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
